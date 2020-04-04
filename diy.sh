e3b2118a31a9483a93f0b5a4c125f3984e531068
fatkun <fatkuns@gmail.com>
2020-04-04 21:28:35 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,12 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+
+# 更改默认主题为Argon
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/mine/luci-theme-argon_new
+sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
+
+# FileBrowser
+git clone https://github.com/project-openwrt/FileBrowser package/mine/FileBrowser
